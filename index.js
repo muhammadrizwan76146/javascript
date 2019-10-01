36e9a5d6830f59d608444b05cc1e745e1d8b0a21
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-02 05:11:53 +0700

    mengenal var, let dan const

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var message = "hello coding";
-let msg1 = "Hallo";
+// var dan let bisa dirubah nilainnya
+var message = "hello coding"; // var nilainya dapat berubah maupun pada penempatan tempat yang berbeda (beda curli bracket)
+let msg1 = "Hallo"; // let nilainya dapat berubah, namun hanya berlaku pada satu tempat.
+
+// const tidak dapat dirubah nilainya, karena nilainya sudah konstant. apabila dirubah akan error.
 const msg2 = "Coding";
 
+// perintah
+if(true){
+    var message = 1;
+    let msg1 = 2;
+    console.log("testing", msg1)
+}
+
+// merubah nilai
+/* msg2 = "rubah 1" */
+
+// perintah
 console.log(message)
+console.log(msg1)
