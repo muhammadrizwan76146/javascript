b5e1afe61942df2f0452633068eb3fd0061a4412
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-06 22:58:51 +0700

    tipe data null

---
+++ null.js
@@ -0,0 +1,8 @@
+// tipe data null, adalah tipe data untuk mendefinisikan atau mencek suatu data ada
+var kosong = "ada";
+kosong = null;
+console.log(kosong);
+
+// tipe data undefined
+var contoh;
+console.log(contoh);
