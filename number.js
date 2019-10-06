ba33ad992ce013ca66ad515a71e9c21003095ee5
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-06 22:30:38 +0700

    variabel number

---
+++ number.js
@@ -0,0 +1,7 @@
+var bulat = 20; // variabel number atau string, tidak perlu menggunakan kutip
+var desimal =35.5;
+var negatif = -10;
+
+console.log(bulat);
+console.log(desimal);
+console.log(negatif);
