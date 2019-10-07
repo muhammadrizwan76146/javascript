53decc0aeb09c80f3a76f11a0772210ae1d18abd
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-07 11:31:10 +0700

    mengembalikan nilai function atau sering disebut return

---
+++ return.js
@@ -0,0 +1,20 @@
+/* return, mengembalikan nilai pada function */
+
+// contoh 1th
+function ambil(){
+    return "terambil";
+}
+
+console.log(ambil());
+
+/*-------------------*/
+
+
+// contoh 2th
+function tangkap(){
+    var tendang = "oke diterima";
+    return tendang;
+}
+
+var terima = tangkap();
+console.log(terima);
