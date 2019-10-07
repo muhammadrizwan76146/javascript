409dfa4fa24461cb88f45baab199fe43c63cc560
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-07 11:47:10 +0700

    membuat parameter

---
+++ parameter.js
@@ -0,0 +1,12 @@
+/* parameter */
+
+function mintaData (x,y,z){
+    console.log(y); // bisa juga menampilkan semua nilainya
+}
+mintaData("hello","how are you","today");
+
+
+/* hasilnya :
+"how are you" karena yng diconsole.log nilai "y" kita juga bisa memasukan nilai yg lain, bisa juga menampilkan semuanya -
+            jika kita mengconsole.log ketiganya "x,y,z" maka hasilnya adalah :
+"hello how are you today" */
