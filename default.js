ffa99a5693ab6706c324b4cc812dd06bcf924276
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-07 11:56:02 +0700

    membuat argumen parameter default

---
+++ default.js
@@ -0,0 +1,9 @@
+function mintaData(x = 10){
+    console.log(x);
+}
+
+
+/* mintaData(20); */                // hasilnya adalah 20
+
+
+mintaData();                        // hasilnya adalah 10, ini adalah contoh default ketika yg di panggil nilainya kosong, "x sudah memiliki nilai default 10"
