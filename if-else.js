2f962a0374af4bf0f99cb230496b1443280e83b1
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-08 21:56:19 +0700

    struktur logika if-else

---
+++ if-else.js
@@ -0,0 +1,28 @@
+/* if-else */
+
+// contoh 1
+function pilihan(){
+    var hidup = "berjuang";
+    if (hidup === "berjuang"){
+        console.log("itulah hidup")
+    }else{
+        console.log("hidup harus berjuang")
+    }
+}
+
+pilihan();
+
+
+// contoh 2
+function jalanSatu(){
+    var nilai = 2;
+    if (nilai === 3){
+        console.log("Benar")
+    }else if(nilai === 2){
+        console.log("almost")
+    }else{
+        console.log("salah")
+    }
+}
+
+jalanSatu();
