5b44c4e5dd52d2715cfde931ab20b7fc2a559e44
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-10 11:59:25 +0700

    switch statement

---
+++ switch.js
@@ -0,0 +1,19 @@
+function testingSwitch() {
+    var nilai = 6;
+
+    switch(nilai) {
+        case 10:
+            console.log("sangat bagus");
+            break;
+        case 8:
+            console.log("Baik");
+            break;
+        case 6:
+            console.log("cukup");
+            break;
+        default:
+            console.log("lainnya");
+    }
+}
+
+testingSwitch();
