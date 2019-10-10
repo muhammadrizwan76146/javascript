c4ab4aad5a1fb8b7c73c0fc2cd9c6c6cd2d37c75
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-10 19:11:50 +0700

    nested loop

---
+++ nested-loop.js
@@ -0,0 +1,9 @@
+/* nested loop, membuat perulangan didalam perulangan */
+
+for (var i=0;i<3;i++){
+    console.log("i ke ",i)
+    for(var j=0;j<2;j++){
+    console.log("j ke ", j)
+    }
+    console.log("")
+}
