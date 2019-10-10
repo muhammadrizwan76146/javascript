92a319aa32119c8c2beebe09d3c2c855e66b1418
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-10 18:58:45 +0700

    perulangan do-while

---
+++ do-while.js
@@ -0,0 +1,8 @@
+/* perulangan do-while */
+
+var i = 1;
+
+do{ // mengeksekusi dari statement nya terlebih dahulu
+    console.log("ini yang ke:", i)
+    i++;
+}while(i<10); // baru mengecek kondisinya
