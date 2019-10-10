baac4c982e8cc0b1f43e3d2e44819397d6b0b464
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-10 19:41:48 +0700

    break and continue

---
+++ break.js
@@ -0,0 +1,15 @@
+// Break
+
+for(var i =0; i<=10;i++){
+    console.log(i)
+    if(i==6)
+    break;
+}
+
+// continue
+
+for(var i = 0; i<=10;i++){
+    if(i==5)
+    continue;
+    console.log(i)
+}
