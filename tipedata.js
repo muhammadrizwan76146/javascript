f5c422126d36b85eaba2235fe98d163558a71f00
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-10 11:46:56 +0700

    ef else berdasarkan tipe data

---
+++ tipedata.js
@@ -0,0 +1,13 @@
+/* if else berdasarkan tipe data */
+
+function tipedata(){
+    var data = 'jakarta';
+
+    if(data === 'jakarta'){
+        return data
+    }else{
+        return 'gagal'
+    }
+}
+
+console.log(tipedata())
