fa06d80e0abc1732529745ee4912e3cc1b84d49d
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-15 00:20:27 +0700

    array didalam object

--- objek.js
+++ objek.js
@@ -2,12 +2,14 @@ function panggilObject(){
     var mobil = {
         type : 'toyota fortunes',
         harga : 'Rp500.000.000,-',
-        warna : 'hitam',
+        warna : ['hitam','putih','grey','silver'] // array didalam object
     }
 
     console.log(mobil);
 
     console.log(mobil.harga); // memanggil lebih spesifik. maka hasilnya hanya akan tapil "Rp500.000.000,-"
+
+    console.log(mobil.warna[2]); // array didalam object. maka hasilnya "grey"
 }
 
 panggilObject();
