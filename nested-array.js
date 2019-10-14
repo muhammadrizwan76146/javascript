a0c8b0cc1c19bd5c133e2a08cd2377068f3594f8
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-14 17:17:14 +0700

    nested array / array didalam array

---
+++ nested-array.js
@@ -0,0 +1,11 @@
+function panggilNestedArray(){
+    var dataNama = [["ari","doni","ria"],
+                ["jakarta","bandung","D.I.Y"],
+                [1994,1995,1996]]
+    
+    console.log(dataNama[0][0]) // hasil= ari
+    console.log(dataNama[1][0]) // hasil= jakarta
+    console.log(dataNama[2][0]) // hasil= 1994
+}
+
+panggilNestedArray();
