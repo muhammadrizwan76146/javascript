e36a0214e2282a3566d8c48dbde881e12cff5e13
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-15 01:36:53 +0700

    property object

---
+++ property.js
@@ -0,0 +1,15 @@
+function panggilObject(){
+
+    var mobil = {
+        model : 'toyota',
+        type : 'fortuner',
+        harga : 'Rp400.000.000,-',
+        warna : 'kuning'
+    }
+
+    mobil.warna = 'hitam'; // merubah object warna
+
+    console.log(mobil);
+}
+
+panggilObject();
