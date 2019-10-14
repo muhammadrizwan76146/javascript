d759ad7227e222a29bb8e153cd829632aa9fa168
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-15 01:45:39 +0700

    menambahkan property

---
+++ tambah-property.js
@@ -0,0 +1,13 @@
+function rubahProperty(){
+    var siswa = {
+        nama : 'Muhammad Rizwan',
+        nim : 111402990,
+        fakultas : 'tehnik informatika',
+    }
+
+    siswa.hobi = 'melukis'; // menambahkan property
+
+    console.log(siswa);
+}
+
+rubahProperty();
