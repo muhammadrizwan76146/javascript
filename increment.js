93c7b52153df026328af11c25e300b4e662bd890
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-17 00:01:30 +0700

    increment dan decrement

---
+++ increment.js
@@ -0,0 +1,19 @@
+
+
+
+// increment
+var a = 0;
+console.log(a)
+a++; // increment
+console.log(a)
+a++; // increment
+console.log(a)
+
+
+// decrement
+var b = 0;
+console.log(b)
+b--; // decrement
+console.log(b)
+b--; // decrement
+console.log(b)
