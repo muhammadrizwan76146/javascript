8270a722c12de2ed6ca6576f2efa3ca225918962
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-17 00:01:00 +0700

    operator aritmatika

---
+++ aritmatika.js
@@ -0,0 +1,9 @@
+var a = 10;
+var b = 20;
+
+// operator aritmatika
+console.log(a-b); // pengurangan
+console.log(a+b); // penjumlahan
+console.log(a*b); // perkalian
+console.log(a/b); // pembagian
+console.log(a%b); // hasil sisa bagi (modulo)
