0238ff6be228d6e9bc01aa6cc4c74baf5244485c
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-16 23:16:06 +0700

    mengecek type data dengan typeof

---
+++ typeof.js
@@ -0,0 +1,11 @@
+/* typeof , untuk mengecek tipe data */
+
+function checkTypeData(){
+    var cek = 'Nama' // tipe datanya string
+
+
+    console.log(typeof(cek)); // typeof untuk mengecek tipe data
+}
+
+
+checkTypeData();
