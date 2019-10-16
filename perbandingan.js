e48b0e6f8d8bd580cfae025074a599fcee875e32
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-17 00:14:14 +0700

    operator perbandingan

---
+++ perbandingan.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b = 11;
+
+
+// operator perbandingan
+console.log(a>b) // lebih besar dari
+console.log(a<b) // kurang dari
+console.log(a>=b) // lebih besar dari sama dengan
+console.log(a<=b) // kurang dari sama dengan
+console.log(a==b) // sama dengan
+console.log(a===b) // sama dengan type data harus sama
+console.log(a!=b) // tidak sama dengan
+console.log(a!==b) // tidak sama dengan type data harus sama
