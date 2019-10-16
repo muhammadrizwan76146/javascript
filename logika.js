4ed56bd21b089b46a50d1b199fcf9cdc16e2a94b
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-17 00:30:54 +0700

    operator logika

---
+++ logika.js
@@ -0,0 +1,6 @@
+var x = 10;
+
+// operator logika
+console.log(x>10 && x<20) // and , true(apabila nilai keduanya benar)
+console.log(x>10 || x<20) // or, true(cukup salah satu yang benar maka nilainya akan true)
+console.log(!true) // not (negasi) nilai kebalikan
