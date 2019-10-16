b4a57870dddd6a95d0b288dbe6be223ac0bda4eb
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-16 21:26:09 +0700

    mengubah object property

---
+++ rubah-property.js
@@ -0,0 +1,15 @@
+function rubahObject(){
+    var mobil = {
+        merek : 'honda',
+        type : 'brio',
+        warna : 'kuning',
+        
+    }
+
+    console.log(mobil);
+
+    mobil.merek = 'toyota';
+    console.log(mobil);
+}
+
+rubahObject();
