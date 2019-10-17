161283bb170eec668e599fef41fd3dfb26d19eff
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-18 05:40:38 +0700

    menghapus index array menggunakan splice

--- array-splice.js
+++ array-splice.js
@@ -1,4 +1,4 @@
-function panggilSplice(){
+/* function panggilSplice(){
     var kota = ['bandung','surabaya','jakarta','malang'];
     console.log(kota);
     kota.splice(2,0,'palembang'); // menempatkan 'palembang' setelah index ke-2 dan '0' artinya tidak menghapus index setelahnya. 
@@ -7,4 +7,14 @@ function panggilSplice(){
     return kota;
 }
 
-console.log(panggilSplice());
+console.log(panggilSplice()); */
+
+function spliceHapus(){
+    var kota = ['sukabumi','bogor','jakarta','bekasi'];
+    console.log(kota);
+    kota.splice(2,1); // yang akan dihapus setelah index ke-2 dan hanya 1
+
+    return kota;
+}
+
+console.log(spliceHapus());
