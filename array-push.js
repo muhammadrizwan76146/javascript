95689007cc8ed8f94596f0bb68d4c7530ce6c1e9
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-18 01:11:49 +0700

    memanipulasi array dengan push

---
+++ array-push.js
@@ -0,0 +1,10 @@
+function arrayPush(){
+    var angka = [1,2,3,4,5,6,7,8,9]; // variabel angka array
+    
+    console.log(angka); // memanggil variabel
+    angka.push(10); // push, manambahkan satu nilai pada array
+
+    return angka; 
+}
+
+console.log(arrayPush());
