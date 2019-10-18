9f99c48e0387f25710a5d8557a876ba2e5a95fb7
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-18 23:40:09 +0700

    pembulatan ceil, floor, dan round

---
+++ pembulatan.js
@@ -0,0 +1,10 @@
+// ceil : membulatkan ke atas
+// floor : membulatkan ke bawah
+// round : membulatkan ke paling dekat
+
+// contoh:
+var x = 12.3;
+
+console.log(Math.ceil(x)); // membulatkan ke atas, jadi 13
+console.log(Math.floor(x)); // membulatkan ke bawah, jadi 12
+console.log(Math.round(x)); // membulatkan ke paling dekat, jadi 12
