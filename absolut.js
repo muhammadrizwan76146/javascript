7081b3601d2ec9eacaf07851f049137dc548f96f
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-19 07:16:34 +0700

    math abs

---
+++ absolut.js
@@ -0,0 +1,7 @@
+var x = 30;
+var y = 40;
+
+console.log(x-y); // hasil: -10
+
+console.log(Math.abs(x-y)); // abs(absolute), membuat hasilnya absolute, meskipun aslinya negatif. hasil: 10
+                            // biasanya absolut ini digunakan untuk mencari selilih
