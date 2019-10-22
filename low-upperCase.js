2be86a9c446858e2a1b27c1f895b58ae098de267
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-22 22:34:31 +0700

    fungsi string lower case dan upper case

---
+++ low-upperCase.js
@@ -0,0 +1,8 @@
+function ubah(){
+    var huruf = "Belajar Javascript";
+
+    console.log(huruf.toLowerCase()); // merubah ke lowerCase atau membuat huruf kecil semua. jadi hasilnya: "belajar javascript"
+                                    // jika ingin membuat hurup besar semua maka kodenya adalah toUpperCase
+}
+
+ubah();
