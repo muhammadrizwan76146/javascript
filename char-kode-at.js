9c923fb3ea10b314f4f47d5e0d81fa0262aeba71
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-22 22:20:03 +0700

    fungsi string char code at

---
+++ char-kode-at.js
@@ -0,0 +1,7 @@
+function kodeAt(){
+    var kode = "Saya belanja dipasar";
+
+    console.log(kode.charCodeAt(4)); // charCodeAt digunakan untuk menampilkan kode unik sebuah karakter atau kode aski sebuah karakter
+}
+
+kodeAt();
