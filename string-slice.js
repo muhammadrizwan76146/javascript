8d61770b81a5273b19ee2abfd1659ee21cc7f234
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-23 21:03:05 +0700

    fungsi string dan slice

--- string-slice.js
+++ string-slice.js
@@ -1,7 +1,9 @@
 function slice(){
     var kata = "aku sedang belajar";
 
-    console.log(kata.slice(3)); // slice ini sama seperti substring, hasil: sedang belajar
+    console.log(kata.slice(3)); // slice ini sama seperti substring, hasil: " sedang belajar"
+    console.log(kata.slice(4,10)); // hasil: "sedang"
+    console.log(kata.slice(-3)); // hasil: "jar"
 }
 
 slice();
