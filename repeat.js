c9ed2e54f6c9439e6447a0c732bb87a1cd86a8a0
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 00:15:27 +0700

    fungsi string repeat

---
+++ repeat.js
@@ -0,0 +1,13 @@
+function stringRepeat(){
+    var kata = "Ayo Belajar ...";
+
+    /* fungsi repeat adalah untuk mengulang sebuah string */
+
+    // contoh mengulah string dari variabel
+    console.log(kata.repeat(3)); // mengulang string dari variabel 'kata' sebanyak 3 kali
+
+    // contoh mengulang string langsung pada console
+    console.log('Ini Buku ...'.repeat(4)); // mgnulang string sebanyak 4 kali
+}
+
+stringRepeat();
