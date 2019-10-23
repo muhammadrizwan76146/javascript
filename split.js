145b21e2b9f60c25c3b99e7552e0d46d80e99b21
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-23 21:11:48 +0700

    fungsi string split

---
+++ split.js
@@ -0,0 +1,8 @@
+function textSplit(){
+    var nama = "Muhammad Rizwan";
+
+    console.log(nama.split()); // merubah string menjadi array
+    console.log(nama.split("")); // merubah string menjadi array per-huruf
+}
+
+textSplit();
