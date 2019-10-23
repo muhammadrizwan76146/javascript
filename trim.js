95e0ab514d122ae0778d0141ef16951fedf63271
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-23 21:22:41 +0700

    fungsi string trim

---
+++ trim.js
@@ -0,0 +1,9 @@
+function textTrim(){
+    var kata = " aku sedang belajar javascript ";
+
+    console.log(kata);
+    console.log(kata.trim()); // trim digunakan untuk menghapus spasi pada awal atau akhir kalimat
+
+}
+
+textTrim();
