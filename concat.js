18150b54b30d511e83f04f0f82ea303b99844aab
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-23 23:05:32 +0700

    fungsi string concat

---
+++ concat.js
@@ -0,0 +1,8 @@
+function textConcat(){
+    var kalimat = "nama saya";
+    var kalimat2 = "Muhammad Rizwan";
+
+    console.log(kalimat.concat(' ',kalimat2,' tinggal di jakarta')); // fungsi concat fungsinya untuk menyambung string, sama seperti +(plus)
+}
+
+textConcat();
