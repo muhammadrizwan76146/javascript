4fab7a3b1824c40dcfae3f04a4a3546d23b7ebb0
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 21:53:29 +0700

    fungsi concat

---
+++ array-concat.js
@@ -0,0 +1,11 @@
+function panggilConcat(){
+    var kota1 = ["jakarta","bandung","malang"];
+    var kota2 = ["surabaya","palembang","samarinda"];
+
+    var gabung = kota1.concat(kota2);
+    return gabung;
+}
+
+console.log(panggilConcat());
+
+/* fungsi concat adalah untuk menggabung */
