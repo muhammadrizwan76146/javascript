4422ba91666c48c79e16859682f5b7f6aa017670
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 22:36:45 +0700

    perintah split

---
+++ array-split.js
@@ -0,0 +1,12 @@
+function panggilSplit(){
+    var kalimat = "aku sedang belajar javascript";
+    console.log(kalimat);
+
+    var result = kalimat.split(" ");
+    return result;
+}
+
+console.log(panggilSplit());
+
+/* fungsi split, untuk merubah string menjadi array berdasarkan yang kita input didalam split. misal " " maka akan terpisah berdasarkan spasi. 
+   atau bisa juga yg lain. contoh "s" maka akan terpisah berdasakan hurup s */
