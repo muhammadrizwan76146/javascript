068afeb28eb32322674776624e508166583f98ef
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 23:03:01 +0700

    perulangan dengan forEach

---
+++ array-forEach.js
@@ -0,0 +1,12 @@
+function panggilForEach(){
+    var data = ['a','b','c'];
+    data.forEach(function(value, index, array){
+        console.log(value);
+        console.log(index);
+        console.log(array);
+    })
+}
+
+panggilForEach();
+
+/* forEach digunakan untuk perulangan */
