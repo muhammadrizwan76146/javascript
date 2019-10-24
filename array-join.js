331b2154ab2a56f8d68f8f596473beb714a71fac
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 22:21:35 +0700

    perintah join

---
+++ array-join.js
@@ -0,0 +1,11 @@
+function panggilJoin(){
+    var kota = ["jakarta","surabaya","bandung","malang"];
+    console.log(kota);
+
+    var result = kota.join(",");
+    return result;
+}
+
+console.log(panggilJoin());
+
+/* fungsi join disini untuk menggabung suatu index pada array menjadi string */
