8b684416abdd7ed9aeb5cfee2a61d21eb4680912
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 22:47:18 +0700

    mengenal indexOf untuk pencarian data array

---
+++ array-indexOf.js
@@ -0,0 +1,7 @@
+function panggilIndexOf(){
+    var kota = ["jakarta","bandung","surabaya","bali","lombok"];
+
+    return kota.indexOf("bandung"); // hasil: 1. karena 'bandung' berada pada index urutan ke-1
+}
+
+console.log(panggilIndexOf());
