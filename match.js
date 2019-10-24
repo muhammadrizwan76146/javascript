2db80ca3244809ab5e45aa7a0616ddf024278523
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 21:09:57 +0700

    fungsi string match

---
+++ match.js
@@ -0,0 +1,9 @@
+function mat(){
+    var kata = "saya pintar memasak nasi goreng";
+    console.log(kata.match("pintar"));
+    console.log(kata.match("goreng"));
+}
+
+mat();
+
+/* match fungsi untuk mencari suatu index pada string (regular ekspresion). dan hasilnya berupa array */
