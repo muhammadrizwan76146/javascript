8c493f32bfe2f11f4ccb9917eecb03ef48d1a92a
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 21:31:25 +0700

    fungsi string replace

---
+++ replace.js
@@ -0,0 +1,10 @@
+function ganti(){
+    var i = "ini isi dari data program";
+
+    console.log(i); // hasil: "ini isi dari data program"
+    console.log(i.replace("program","penjualan")) // hasil: "ini isi dari data penjualan". isi string dari variabel i 'perogram' menjadi 'penjualan'
+}
+
+ganti();
+
+/* fungsi replace adalah untuk menggati isi dari suatu string */
