180aaec328b7928b69adc85e9d4a66994d36d4cf
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 20:38:36 +0700

    indexOf untuk mencek urutan suatu string pada index

---
+++ indexOf.js
@@ -0,0 +1,9 @@
+function indek(){
+    var kata = "Saya sudah makan nasi hari ini";
+    console.log(kata.indexOf("makan")); // hasil: 11. karena makan dimulai dari index urutan ke 11
+    console.log(kata.indexOf("Saya")); // hasil: 0. karena Saya dimulai pada index pertama yaitu 0
+}
+
+indek();
+
+/* funsi indexOf, adalah untuk mengeCek sebuah String dimulai dari index ke-berapa. */
