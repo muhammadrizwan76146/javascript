543232f6e98b8d584efa00cce4492ff11a807f0e
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 21:44:21 +0700

    perintah reverse

---
+++ array-reverse.js
@@ -0,0 +1,12 @@
+function panggilReverse(){
+    var kota = ["jakart","bandung","joga","malang"];
+    console.log(kota);
+    return kota.reverse(); /* reverse cara langsung */
+
+    // var balik = kota.reverse();  /* cara kedua dengan membuat variabel baru */
+    // return balik;
+}
+
+console.log(panggilReverse());
+
+/* fungsi reverse untuk membalik suatu array */
