97d7c3b64bc899eeeb34abdab8f56989c7350b06
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-25 23:04:51 +0700

    fungsi array isFinite

--- array-isfinite.js
+++ array-isfinite.js
@@ -2,7 +2,7 @@ function panggilIsFinite(){
     var a = isFinite([1,2,3,4,5]);
     var b = isFinite(["jakarta","bandung","surabaya"]);
     var c = isFinite(1,2,3,"hello","niomic");
-    var d = isFinite(-25232.42,9,546,95,2);
+    var d = isFinite(-25232.42,9,546,95,2); 
 
     console.log(a);
     console.log(b);
