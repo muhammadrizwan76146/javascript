948207bbe9abd0dbe2086a792cfb8b0eb5f028c0
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-25 23:03:55 +0700

    isFinite, harus mempunyai data tidak terbatas

---
+++ array-isfinite.js
@@ -0,0 +1,13 @@
+function panggilIsFinite(){
+    var a = isFinite([1,2,3,4,5]);
+    var b = isFinite(["jakarta","bandung","surabaya"]);
+    var c = isFinite(1,2,3,"hello","niomic");
+    var d = isFinite(-25232.42,9,546,95,2);
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+}
+
+panggilIsFinite();
