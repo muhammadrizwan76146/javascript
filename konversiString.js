0a9b0be050115c07b8ea852f3e0f21ee66947d7a
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-26 00:16:30 +0700

    mengkonversi data menjadi string

---
+++ konversiString.js
@@ -0,0 +1,25 @@
+function konversiString(){
+    var a = 123;
+    var b = 98.70;
+    var c = "NIOMICC!";
+    var d = true;
+
+    hasil1 = String(a);
+    console.log(hasil1);
+
+    hasil2 = String(b);
+    console.log(hasil2);
+
+    hasil3 = hasil1 + hasil2;
+    console.log(hasil3);
+
+    hasil4 = String(c);
+    console.log(hasil4);
+
+    hasil5 = String(d);
+    console.log(hasil5);
+}
+
+konversiString();
+
+/* mengkonversi sebuah data menjadi string */
