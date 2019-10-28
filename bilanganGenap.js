7b82a0277c046df6ccff7baeca1a65ab97709431
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-28 22:46:02 +0700

    bilangan genap

---
+++ bilanganGenap.js
@@ -0,0 +1,11 @@
+function panggilBilanganGenap() {
+    var bilanganGenap = [];
+    for ( var i=0; i<10; i++) {
+        if(i%2 === 0){
+            bilanganGenap.push(i);
+        }
+    }
+    return bilanganGenap;
+}
+
+console.log(panggilBilanganGenap());
