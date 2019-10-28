845252193c08be5082ba063cdf0b718a6638b849
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-28 23:19:46 +0700

    forEach

---
+++ forEach.js
@@ -0,0 +1,10 @@
+function panggilForEach() {
+    var data = ['a','b','c'];
+    data.forEach(function(value, index, array){
+        console.log(value);
+        console.log(index);
+        console.log(array);
+    })
+}
+
+panggilForEach();
