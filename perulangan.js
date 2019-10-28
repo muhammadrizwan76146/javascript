072a23e17474fac6fc232982816a53fca022e734
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-28 23:36:32 +0700

    perulangan for

---
+++ perulangan.js
@@ -0,0 +1,10 @@
+function panggilPerulangan() {
+    var dataKota = ['jakarta','bandung','yogyakarta'];
+    for(var i=0; i<dataKota.length; i++) {
+        console.log(dataKota[i]);
+        console.log(i);
+        console.log(dataKota);
+    }
+}
+
+panggilPerulangan();
